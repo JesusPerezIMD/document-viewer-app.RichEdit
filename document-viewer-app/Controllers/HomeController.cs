65d9944c00cf4229e31ab364378768f0d2fe0503
Jesús Alejandro Pérez Ramírez <dual.jesus.perez@gmail.com>
2023-05-02 18:56:02 -0500

    Commit1. 2/May/2023 - 6:55PM

--- document-viewer-app/Controllers/HomeController.cs
+++ document-viewer-app/Controllers/HomeController.cs
@@ -6,27 +6,41 @@ namespace document_viewer_app.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IHostApplicationLifetime _appLifetime;
+
+        public HomeController(IHostApplicationLifetime appLifetime)
         {
-            _logger = logger;
+            _appLifetime = appLifetime;
         }
-
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public async Task<IActionResult> DescargarArchivo(string nombreArchivo, string urlDescarga)
         {
-            return View();
-        }
+            // Crear una instancia de HttpClient
+            using var httpClient = new HttpClient();
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            // Descargar el archivo y almacenarlo temporalmente en el proyecto
+            var rutaArchivo = $"ArchivosTemporales/{nombreArchivo}";
+            var respuesta = await httpClient.GetAsync(urlDescarga);
+            var contenidoArchivo = await respuesta.Content.ReadAsByteArrayAsync();
+            System.IO.File.WriteAllBytes(rutaArchivo, contenidoArchivo);
+
+            // Establecer un método de devolución de llamada para eliminar el archivo cuando se cierre el proyecto
+            _appLifetime.ApplicationStopped.Register(() =>
+            {
+                if (System.IO.File.Exists(rutaArchivo))
+                {
+                    System.IO.File.Delete(rutaArchivo);
+                }
+            });
+
+            // Descargar el archivo al navegador del usuario
+            var stream = new FileStream(rutaArchivo, FileMode.Open);
+            return File(stream, "application/octet-stream", nombreArchivo);
         }
     }
 }
